c616aab88d4ab995dd6b2550d52d3b14309b0c0d
Masato Ohira <ohira.ekzm@gmail.com>
2022-12-19 14:35:01 +0900

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,15 +15,6 @@ module.exports = {
     'no-unused-vars': 1,
     'no-irregular-whitespace': 1,
     'max-len': 0,
-    // 'quotes': [1, 'single', {"avoidEscape": true}],
-    quotes: [1, 'single', { allowTemplateLiterals: true }],
-
-    // セミコロン省略
-    semi: ['error', 'never', { beforeStatementContinuationChars: 'never' }],
-    'semi-spacing': ['error', { after: true, before: false }],
-    'semi-style': ['error', 'first'],
-    'no-extra-semi': 'error',
-    'no-unexpected-multiline': 'error',
-    'no-unreachable': 'error',
+    'react/no-unknown-property': ['error', { ignore: ['css'] }],
   },
 }
